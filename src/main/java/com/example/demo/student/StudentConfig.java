835d0b684e13b2d10b7441c9a27e733951566162
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2023-05-12 17:52:33 -0700

    init

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student alex = new Student(
+                    "Alex",
+                    LocalDate.of(1994, Month.AUGUST, 19),
+                    "alex@gmail.com"
+            );
+
+            Student bob = new Student(
+                    "Bob",
+                    LocalDate.of(2004, Month.JANUARY, 2),
+                    "Bob@gmail.com"
+            );
+
+            repository.saveAll(
+                    List.of(alex, bob)
+            );
+        };
+    }
+}
